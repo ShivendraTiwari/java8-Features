735ccfa61e4f0f6a3e97a781868ba15bc1ffba5f
Shivendra Tiwari <60955502+ShivendraTiwari@users.noreply.github.com>
2020-04-18 08:11:32 +0530

    Add files via upload

---
+++ ConsumerInterface.java
@@ -0,0 +1,28 @@
+package com.shivendra.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+//class ConsImp implements Consumer<Integer> {
+//
+//	public void accept(Integer i) {
+//		System.out.println(i);
+//	}
+//}
+
+public class ConsumerInterface {
+
+	public static void main(String[] args) {
+
+		List<Integer> values = Arrays.asList(4, 5, 6, 7, 8);
+		// Consumer<Integer> c =new Consumer<Integer>() {
+//			public void accept(Integer i) {
+//				System.out.println(i);
+//			}
+//		};
+		Consumer<Integer> c = i -> System.out.println(i);
+		values.forEach(c);
+	}
+
+}
